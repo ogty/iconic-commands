05fafbf593b0480a4d1fa2c7646008a879f7bc29
ogty <mofuiti1803@gmail.com>
2022-06-27 03:26:27 +0900

    changed to be installed under different conditions

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 if [ which brew > /dev/null ]; then else
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if [ which rsvg-convert > /dev/null ]; then else
   brew install librsvg
 fi
 
 if [ which npm > /dev/null ]; then else
   brew install npm
+fi
+
+if [ which fileicon > /dev/null ]; then else
   npm install -g fileicon
 fi
 
